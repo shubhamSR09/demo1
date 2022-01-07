9e0682e3b1b5cae6f9a3479f1b879a0e9a7cfb21
Shubham Kumar <67828564+shubhamSR09@users.noreply.github.com>
2022-01-07 11:37:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,42 @@ import numpy as np
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/hello")
-def index():
-	flash("what's your name?")
-	return render_template("index.html")
+@app.route('/hello')
+def home():
+	return render_template('new_diabetes.html')
 
-@app.route("/greet", methods=['POST', 'GET'])
-def greeter():
-	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    my_prediction=''
+    sr=''
+    if request.method=="POST":
+
+
+        rs1=int(request.form["age"])
+        rs2=int(request.form["sex"])
+        rs3=request.form.get("Polydipsia")
+        rs4=request.form.get("Sudden Weigth loss")
+        rs5=request.form.get("Polyphagia")
+        rs6=request.form.get("Gental Thrush")
+        rs7=request.form.get("Visual Blurring")
+        rs14=request.form.get("Itching")
+        rs8=request.form.get("Irritability")
+        rs9=request.form.get("Delayed Healing")
+        rs10=request.form.get("Partial Paresis")
+        rs11=request.form.get("Muscle Stiffness")
+        rs12=request.form.get("Alopecia")
+        rs13=request.form.get("Obesity")
+        
+        
+        data=np.array([[rs1,rs2,rs3,rs4,rs5,rs6,rs7,rs8,rs9,rs10,rs11,rs12,rs13,rs14]])
+        my_prediction=lr.predict(data)
+        print(my_prediction)
+        
+
+        
+    return render_template('diabetes_result.html', prediction=my_prediction)
+    #return "than you"
+    
+
+
